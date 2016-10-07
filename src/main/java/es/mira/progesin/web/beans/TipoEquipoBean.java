063388e6c42852c00f0962e6c7d9c2c526f80227
MOV06253\admin <alejandro.vaquerizo@ezentis.com>
2016-10-07 14:27:16 +0200

    prueba

--- src/main/java/es/mira/progesin/web/beans/TipoEquipoBean.java
+++ src/main/java/es/mira/progesin/web/beans/TipoEquipoBean.java
@@ -4,15 +4,20 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.mail.MessagingException;
 
 import org.apache.log4j.Logger;
+import org.primefaces.event.RowEditEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import es.mira.progesin.persistence.entities.CuerpoEstado;
 import es.mira.progesin.persistence.entities.TipoEquipo;
 import es.mira.progesin.services.ITipoEquipoService;
+import es.mira.progesin.util.FacesUtilities;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,10 +28,14 @@ import lombok.Setter;
 public class TipoEquipoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private TipoEquipo equipo;
 
 	static Logger LOG = Logger.getLogger(TipoEquipoBean.class);
 
 	private List<TipoEquipo> listaTipoEquipo;
+	
+	private String tipoNuevo;
 
 	@Autowired
 	ITipoEquipoService tipoEquipoService;
@@ -40,8 +49,8 @@ public class TipoEquipoBean implements Serializable {
 		return "/equipos/listadoEquipos";
 	}
 
-	public void eliminarTipoEquipo(Integer idTipoEquipo) {
-		tipoEquipoService.delete(idTipoEquipo);
+	public void eliminarEquipo(TipoEquipo equipo) {
+		tipoEquipoService.delete(equipo.getIdTipoEquipo());
 		this.listaTipoEquipo = null;
 		listaTipoEquipo = (List<TipoEquipo>) tipoEquipoService.findAll();
 	}
@@ -50,7 +59,39 @@ public class TipoEquipoBean implements Serializable {
 	public void init() throws MessagingException {
 
 		listaTipoEquipo = (List<TipoEquipo>) tipoEquipoService.findAll();
+		
 
 	}
+	public void altaTipo() {
+		TipoEquipo equipo = new TipoEquipo();
+		equipo.setDescripcion(tipoNuevo);
+		try {
+			if (tipoEquipoService.save(equipo) != null) {
+				FacesUtilities.setMensajeConfirmacionDialog(FacesMessage.SEVERITY_INFO, "Alta",
+						"El tipo de equipo ha sido creado con éxito");
+			}
+		}
+		catch (Exception e) {
+			FacesUtilities.setMensajeConfirmacionDialog(FacesMessage.SEVERITY_ERROR, "Error",
+					"Se ha producido un error al dar de alta el tipo de equipo, inténtelo de nuevo más tarde");
+			// TODO log de errores
+		}
+		listaTipoEquipo = (List<TipoEquipo>) tipoEquipoService.findAll();
+		// TODO generar alerta / notificación
+	}
+	public void onRowEdit(RowEditEvent event) {
+		TipoEquipo equipo = (TipoEquipo) event.getObject();
+		tipoEquipoService.save(equipo);
+		FacesMessage msg = new FacesMessage("Tipo de equipos  modificado", equipo.getDescripcion());
+		FacesContext.getCurrentInstance().addMessage("msgs", msg);
+	}
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Modificación cancelada",
+				((TipoEquipo) event.getObject()).getDescripcion());
+		FacesContext.getCurrentInstance().addMessage("msgs", msg);
+	}
+	
+
+
 
 }
