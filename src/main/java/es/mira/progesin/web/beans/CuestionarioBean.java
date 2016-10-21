8f0d6eed0f68e3a2ca15f3ae9f33d44450ecdeb4
Dragos Sterpu <Dragos Sterpu>
2016-10-21 07:59:32 +0200

    solicitud documentacion cuestionario jasper

--- src/main/java/es/mira/progesin/web/beans/CuestionarioBean.java
+++ src/main/java/es/mira/progesin/web/beans/CuestionarioBean.java
@@ -1,123 +1,212 @@
 package es.mira.progesin.web.beans;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.context.annotation.Scope;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
-import net.sf.jasperreports.export.Exporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import es.mira.progesin.persistence.entities.DatosJasper;
+import es.mira.progesin.persistence.entities.Notificacion;
+import es.mira.progesin.persistence.entities.PreEnvioCuestionario;
+import es.mira.progesin.persistence.entities.RegActividad;
+import es.mira.progesin.persistence.entities.enums.EstadoRegActividadEnum;
+import es.mira.progesin.services.IModeloCuestionarioService;
+import es.mira.progesin.services.INotificacionService;
+import es.mira.progesin.services.IRegActividadService;
+import es.mira.progesin.util.DescargasHelper;
+import es.mira.progesin.util.Utilities;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Component("cuestionarioBean")
-@RequestScoped
-@Scope("session")
+@SessionScoped
+
 public class CuestionarioBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	RegActividad regActividad = new RegActividad();
+
+	private final String NOMBRESECCION = "Generación de plantilla jasper";
+
+	@Autowired
+	IRegActividadService regActividadService;
+
+	@Autowired
+	INotificacionService notificacionService;
+
+	@Autowired
+	IModeloCuestionarioService modeloCuestionarioService;
+
+	List<PreEnvioCuestionario> listadoPreEnvioCuestionarios;
+
+	private StreamedContent file;
+
+	private DatosJasper model;
+
+	// Url de la plantilla jasper
+	private static final String RUTA_JASPER = "jasper/gcZonaPluriprovincial.jasper";
+
 	public String creaCuestionario() {
 
+		return "/cuestionarios/preenvio";
+
+	}
+
+	public String execute() {
 		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-
-		ServletContext context = (ServletContext) externalContext.getContext();
-
-		HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-
-		HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+		HttpSession session = (HttpSession) externalContext.getSession(true);
+		final DatosJasper datosjasper = new DatosJasper();
+		datosjasper.setNombre("GC_ZONA_PLURI_PROVINCIAL");
+		datosjasper.setUrl(RUTA_JASPER);
 		try {
-			Map<String, Object> parameters = new HashMap<String, Object>();
-			parameters.put("asunto", "Comunicando InspeccÃ³n General PeriÃ³dica y solicitando documentaciÃ³n");
-			parameters.put("numeroReferencia", "SA/IGP/01/2016");
-			parameters.put("correoApoyo", "apoyo_ipss@interior.es");
-			parameters.put("fechaAntes", "09/05/2016");
-			parameters.put("destinatario", "SR. GENERAL, JEFE DE LA ZONA DE LA GUARDIA CIVIL DE LA CC AA DE");
-			parameters.put("tipoDestinatario", "Policia");
-			parameters.put("tipoInspeccion", "general periÃ³dica");
-			parameters.put("identificadorTrimestre", "tercer cuatrimestre del aÃ±o 2016");
-			// parameters.put("fecha", "18 de abril de 2016");
-			// parameters.put("fechaLimit", "11/01/2017");
-			parameters.put("jefeServicios", "EL JEFE DE LOS SERVICIOS DE INSPECCIÃ“N");
-			parameters.put("nombreJefeServicios", "JosÃ© LuÃ­s DomÃ­nguez Alonso");
-			JasperReport jasperReport = getCompiled("solicitudDevolucion1");
+			DescargasHelper.preparaDescargaJasper(datosjasper, session);
+			String descripcion = "Creación de nuevo cuestionario previo. Nombre del cuestionario"
+					+ datosjasper.getNombre() + " Usuario creación : "
+					+ SecurityContextHolder.getContext().getAuthentication().getName();
+			// Guardamos la actividad en bbdd
+			saveReg(descripcion, EstadoRegActividadEnum.ALTA.name(),
+					SecurityContextHolder.getContext().getAuthentication().getName());
 
-			String[] pageFile = new String[3];
-			pageFile[0] = "preCuestionario_subreport1";
-			pageFile[1] = "preCuestionario_subreport2";
-			pageFile[2] = "preCuestionario_subreport3";
-
-			JasperReport[] jasperSubReport = new JasperReport[3];
-			for (int i = 0; pageFile.length > i; i++) {
-				jasperSubReport[i] = getCompiled(pageFile[i]);
-			}
-			parameters.put("preCuestionario_subreport1", jasperSubReport[0]);
-			parameters.put("preCuestionario_subreport2", jasperSubReport[1]);
-			parameters.put("preCuestionario_subreport3", jasperSubReport[2]);
-			// JasperReport reporte = (JasperReport) JRLoader
-			// .loadObjectFromFile("C:/Users/Admin/Desktop/jasper/preCuestionario.jasper");
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters);
-			byte[] bytes = JasperExportManager.exportReportToPdf(jasperPrint);
-			String fileJasper = "C:/Users/Admin/Desktop/jasper/solicitudDevolucion.jasper";
-			response.setContentType("application/pdf");
-			response.setContentLength(bytes.length);
-			response.getOutputStream().write(bytes, 0, bytes.length);
-			response.getOutputStream().flush();
-			response.getOutputStream().close();
-			// JasperPrint print = JasperFillManager.fillReport(fileJasper, null, new JREmptyDataSource());
-			// JasperViewer jviewer = new JasperViewer(print, false);
-			// jviewer.setVisible(true);
-
-			// Exporta el informe a PDF
-			JasperExportManager.exportReportToPdfFile(jasperPrint,
-					"C:/Users/Admin/Desktop/jasper/solicitudDevolucion1.pdf");
-			// Para visualizar el pdf directamente desde java
-			// JasperViewer.viewReport(print, false);
-			Exporter exporter = new JRDocxExporter();
-			exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-
-			File exportReportFile = new File("C:/Users/Admin/Desktop/jasper/solicitudDevolucion.docx");
-
-			exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(exportReportFile));
-
-			exporter.exportReport();
+			// Guardamos la notificacion en bbdd
+			saveNotificacion(descripcion, EstadoRegActividadEnum.ALTA.name(),
+					SecurityContextHolder.getContext().getAuthentication().getName());
 		}
-		catch (
-
-		Exception e) {
-			e.printStackTrace();
+		catch (Exception e) {
+			// Guardamos loe posibles errores en bbdd
+			altaRegActivError(e);
 		}
+		return null;
 
-		return "/registro/registro";
+	}
+
+	public String limpiar() {
+		// this.model = null;
+		return "/cuestionarios/preenvio";
 	}
 
 	@PostConstruct
 	public void init() {
 
+		// insertar();
+		listadoPreEnvioCuestionarios = modeloCuestionarioService.findAllPre();
+
 	}
 
-	private JasperReport getCompiled(String fileName) throws JRException {
-		InputStream is = FacesContext.getCurrentInstance().getExternalContext()
-				.getResourceAsStream("C:\\Users\\Admin\\Desktop\\jasper\\" + fileName + ".jrxml");
-		JasperReport jasperReport = JasperCompileManager.compileReport(is);
-		return jasperReport;
+	public void insertar() {
+		try {
+
+			File directory = new File("C:\\Users\\Admin\\Desktop\\Documentación IPSS\\preenvioCuestionarios");
+
+			File[] listaFicheros = directory.listFiles();
+			for (int i = 0; i < listaFicheros.length; i++) {
+				File fichero = listaFicheros[i];
+
+				// Obtiene el contenido del fichero en []bytes
+				byte[] data = Files.readAllBytes(fichero.toPath());
+				PreEnvioCuestionario cuestionario = new PreEnvioCuestionario();
+				cuestionario.setCodigo("codigo");
+				cuestionario.setDescripcion(
+						fichero.getName().substring(0, fichero.getName().lastIndexOf('.')).toUpperCase());
+				cuestionario.setNombreFichero(fichero.getName());
+				System.out.println(fichero.getName().substring(fichero.getName().lastIndexOf('.') + 1));
+				cuestionario.setExtension(
+						fichero.getName().substring(fichero.getName().lastIndexOf('.') + 1).toLowerCase());
+				// Blob fichero = Hibernate.getLobCreator(sessionFactory.openSession()).createBlob(data);
+				cuestionario.setFichero(data);
+				modeloCuestionarioService.savePre(cuestionario);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	/**
+	 * Método que recoge los valores introducidos en el formulario y da de alta un equipo normal en la BBDD
+	 * @return
+	 */
+	public String enviarPreCuestionario(PreEnvioCuestionario cuestionario) {
+
+		return "/cuestionarios/preenvio";
+	}
+
+	public void descargarFichero(PreEnvioCuestionario cuestionario) {
+		try {
+			InputStream stream = new ByteArrayInputStream(cuestionario.getFichero());
+			String contentType = "application/msword";
+			if ("pdf".equals(cuestionario.getExtension())) {
+				contentType = "application/pdf";
+			}
+			else if (cuestionario.getExtension().startsWith("xls")) {
+				contentType = "application/x-msexcel";
+			}
+			file = new DefaultStreamedContent(stream, contentType, cuestionario.getNombreFichero());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// ************* Alta mensajes de notificacion, regActividad y alertas Progesin ********************
+	/**
+	 * @param descripcion
+	 * @param tipoReg
+	 * @param username
+	 */
+	private void saveReg(String descripcion, String tipoReg, String username) {
+		RegActividad regActividad = new RegActividad();
+		regActividad.setTipoRegActividad(tipoReg);
+		regActividad.setUsernameRegActividad(username);
+		regActividad.setFechaAlta(new Date());
+		regActividad.setNombreSeccion(NOMBRESECCION);
+		regActividad.setDescripcion(descripcion);
+		regActividadService.save(regActividad);
+	}
+
+	/**
+	 * @param descripcion
+	 * @param tipoReg
+	 * @param username
+	 */
+	private void saveNotificacion(String descripcion, String tipoNotificacion, String username) {
+		Notificacion notificacion = new Notificacion();
+		notificacion.setTipoNotificacion(tipoNotificacion);
+		notificacion.setUsernameNotificacion(username);
+		notificacion.setNombreSeccion(NOMBRESECCION);
+		notificacion.setFechaAlta(new Date());
+		notificacion.setDescripcion(descripcion);
+		notificacionService.save(notificacion);
+	}
+
+	/**
+	 * @param e
+	 */
+	private void altaRegActivError(Exception e) {
+		regActividad.setTipoRegActividad(EstadoRegActividadEnum.ERROR.name());
+		String message = Utilities.messageError(e);
+		regActividad.setFechaAlta(new Date());
+		regActividad.setNombreSeccion(NOMBRESECCION);
+		regActividad.setUsernameRegActividad(SecurityContextHolder.getContext().getAuthentication().getName());
+		regActividad.setDescripcion(message);
+		regActividadService.save(regActividad);
+	}
+	// ************* Alta mensajes de notificacion, regActividad y alertas Progesin END********************
 }
