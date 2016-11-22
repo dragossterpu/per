ca472685e06239909c29dae0a60d80e0fc6a4df7
AVANZIT\rastudillo <ruben.astudillo@ezentis.com>
2016-11-22 16:28:04 +0100

    Añadida la opción de envío de correo con adjunto

--- src/main/java/es/mira/progesin/util/ICorreoElectronico.java
+++ src/main/java/es/mira/progesin/util/ICorreoElectronico.java
@@ -1,9 +1,13 @@
 package es.mira.progesin.util;
 
+import java.io.File;
+
 public interface ICorreoElectronico { 
 	
 	public void envioCorreo (String destino, String asunto, String cuerpo);
-	
 	public void envioCorreo (String[] destino, String asunto, String cuerpo);
+	
+	public void envioCorreoAdjuntos (String destino, String asunto, String cuerpo, File adjunto);
+	public void envioCorreoAdjuntos (String[] destino, String asunto, String cuerpo, File adjunto);
 	
 }
