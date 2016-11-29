76372d47a9378ad8f18a9b5dee5f9ec8025f4dbf
AVANZIT\silvia.perez <silvia.perez@ezentis.com>
2016-11-29 08:41:09 +0100

    Borrar código que no pinta nada

--- java/es/mira/progesin/persistence/repositories/IAlertaRepository.java
+++
@@ -1,8 +0,0 @@
-package es.mira.progesin.persistence.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import es.mira.progesin.persistence.entities.Alerta;
-
-public interface IAlertaRepository extends CrudRepository<Alerta, Integer> {
-}
--- java/es/mira/progesin/services/IPuestoTrabajoService.java
+++
@@ -1,7 +0,0 @@
-package es.mira.progesin.services;
-
-import es.mira.progesin.persistence.entities.PuestoTrabajo;
-
-public interface IPuestoTrabajoService {
-	Iterable<PuestoTrabajo> findAll();
-}
