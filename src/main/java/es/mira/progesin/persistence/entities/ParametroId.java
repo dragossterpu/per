a687fc8c11657d22c2d6c55712e5d773da0f4925
AVANZIT\rgarciao <ramon.garcia@ezentis.com>
2016-12-21 11:54:43 +0100

    Forzar tamaño varchar 4000 para evitar que lo cree por defecto con límite de 255 caracteres

--- src/main/java/es/mira/progesin/persistence/entities/ParametroId.java
+++ src/main/java/es/mira/progesin/persistence/entities/ParametroId.java
@@ -2,6 +2,7 @@ package es.mira.progesin.persistence.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
@@ -20,9 +21,9 @@ public class ParametroId implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	String seccion;
-	
+
 	String clave;
 
+	@Column(length = 4000)
 	String valor;
 }
-
