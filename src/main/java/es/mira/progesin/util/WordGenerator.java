4ab22f6917563bf51dbb9f1f27470a158ddbaa86
AVANZIT\silvia.perez <silvia.perez@ezentis.com>
2017-01-17 10:16:14 +0100

    - Se añade documentación a la clase WordGenerator - Se limita la generación del documento word para que sólo salga el icono cuando vienen de la búsqueda de cuestionarios personalizados

--- src/main/java/es/mira/progesin/util/WordGenerator.java
+++ src/main/java/es/mira/progesin/util/WordGenerator.java
@@ -47,6 +47,12 @@ import es.mira.progesin.persistence.entities.enums.ContentTypeEnum;
 import es.mira.progesin.persistence.repositories.IConfiguracionRespuestasCuestionarioRepository;
 import es.mira.progesin.persistence.repositories.IPreguntaCuestionarioRepository;
 
+/**
+ * Clase para la generación de documentos DOCX
+ * 
+ * @author EZENTIS
+ *
+ */
 @Component("wordGenerator")
 public class WordGenerator {
 
@@ -61,18 +67,30 @@ public class WordGenerator {
 	// Azul claro en hexadecimal
 	public static final String COLOR_CELDA_TABLA = "bbd2f7";
 
+	public static final String TIPO_RESPUESTA_TABLA = "TABLA";
+
 	@Autowired
 	private IPreguntaCuestionarioRepository preguntasRepository;
 
 	@Autowired
 	IConfiguracionRespuestasCuestionarioRepository configuracionRespuestaRepository;
 
+	/**
+	 * Genera un documento DOCX a partir de un cuestionario personalizado, mostrando el texto de las preguntas y, en
+	 * aquellos casos donde el tipo de respuesta es de tipo TABLA o MATRIZ, dibujando las tablas/matrices de las
+	 * respuestas. En el caso de los tipo de respuesta RADIO, las opciones posibles se añaden al texto de la pregunta
+	 * 
+	 * @param cuestionarioPersonalizado
+	 * @return StreamedContent Stream para descargar el fichero en la ventana del navegador
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 */
 	public StreamedContent crearDocumentoCuestionarioPersonalizado(CuestionarioPersonalizado cuestionarioPersonalizado)
 			throws InvalidFormatException, IOException {
 		// Blank Document
 		XWPFDocument doc = new XWPFDocument();
 
-		crearCabeceraCuestionario(doc);
+		crearCabeceraCuestionario(doc, LOGO_MININISTERIO_INTERIOR, LOGO_IPSS);
 
 		XWPFParagraph parrafo = doc.createParagraph();
 		// XWPFRun is used to add a region of text to the paragraph.
@@ -143,7 +161,18 @@ public class WordGenerator {
 				"CuestionarioPersonalizado.docx");
 	}
 
-	private void crearCabeceraCuestionario(XWPFDocument doc) throws InvalidFormatException, IOException {
+	/**
+	 * Crea la cabecera del documento
+	 * 
+	 * @see #crearDocumentoCuestionarioPersonalizado(CuestionarioPersonalizado)
+	 * @param doc XWPFDocument Documento donde se insertará la cabecera
+	 * @param imagen1 Imagen situada en la parte superior izquierda de la cabecera
+	 * @param imagen2 Imagen situada en la parte superior derecha de la cabecera
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 */
+	private void crearCabeceraCuestionario(XWPFDocument doc, String imagen1, String imagen2)
+			throws InvalidFormatException, IOException {
 		// create header start
 		CTSectPr sectPr = doc.getDocument().getBody().addNewSectPr();
 		XWPFHeaderFooterPolicy headerFooterPolicy = new XWPFHeaderFooterPolicy(doc, sectPr);
@@ -155,9 +184,8 @@ public class WordGenerator {
 
 		XWPFRun run = paragraph.createRun();
 
-		XWPFPicture picture = run.addPicture(new FileInputStream(LOGO_MININISTERIO_INTERIOR),
-				XWPFDocument.PICTURE_TYPE_PNG, LOGO_MININISTERIO_INTERIOR, Units.toEMU(177 * 0.6),
-				Units.toEMU(90 * 0.6));
+		XWPFPicture picture = run.addPicture(new FileInputStream(imagen1), XWPFDocument.PICTURE_TYPE_PNG, imagen1,
+				Units.toEMU(177 * 0.6), Units.toEMU(90 * 0.6));
 
 		String blipID = "";
 		for (XWPFPictureData picturedata : header.getAllPackagePictures()) {
@@ -171,7 +199,7 @@ public class WordGenerator {
 		setTabStop(paragraph, STTabJc.Enum.forString("right"), pos2);
 
 		run.addTab();
-		XWPFPicture picture2 = run.addPicture(new FileInputStream(LOGO_IPSS), XWPFDocument.PICTURE_TYPE_PNG, LOGO_IPSS,
+		XWPFPicture picture2 = run.addPicture(new FileInputStream(imagen2), XWPFDocument.PICTURE_TYPE_PNG, imagen2,
 				Units.toEMU(264 * 0.6), Units.toEMU(85 * 0.6));
 		blipID = "";
 		for (XWPFPictureData picturedata : header.getAllPackagePictures()) {
@@ -180,6 +208,13 @@ public class WordGenerator {
 		picture2.getCTPicture().getBlipFill().getBlip().setEmbed(blipID);
 	}
 
+	/**
+	 * Añade tabulación en un párrafo
+	 * @see #crearCabeceraCuestionario(XWPFDocument, String, String)
+	 * @param oParagraph
+	 * @param oSTTabJc
+	 * @param oPos
+	 */
 	private void setTabStop(XWPFParagraph oParagraph, STTabJc.Enum oSTTabJc, BigInteger oPos) {
 		CTP oCTP = oParagraph.getCTP();
 		CTPPr oPPr = oCTP.getPPr();
@@ -197,6 +232,13 @@ public class WordGenerator {
 		oTabStop.setPos(oPos);
 	}
 
+	/**
+	 * Inserta las preguntas relacionadas con un área en el documento
+	 * 
+	 * @see #crearDocumentoCuestionarioPersonalizado(CuestionarioPersonalizado)
+	 * @param listaPreguntasArea Lista de preguntas asociadas a un área
+	 * @param doc XWPFDocument donde se insertarán las preguntas
+	 */
 	private void crearPreguntasPorArea(List<PreguntasCuestionario> listaPreguntasArea, XWPFDocument doc) {
 		XWPFParagraph parrafo;
 		XWPFRun texto;
@@ -207,7 +249,7 @@ public class WordGenerator {
 			texto.setBold(true);
 			texto.setFontFamily(FONT_FAMILY);
 			if (pregunta.getTipoRespuesta().startsWith("RADIO")) {
-				texto.setText(pregunta.getPregunta() + " (valores posibles : " + getValoresRadioButton(pregunta) + ")");
+				texto.setText(pregunta.getPregunta() + " (valores posibles: " + getValoresRadioButton(pregunta) + ")");
 			}
 			else {
 				texto.setText(pregunta.getPregunta());
@@ -216,19 +258,27 @@ public class WordGenerator {
 			parrafo.setSpacingBeforeLines(200);
 			parrafo.addRun(texto);
 
-			if (pregunta.getTipoRespuesta().startsWith("TABLA") || pregunta.getTipoRespuesta().startsWith("MATRIZ")) {
+			if (pregunta.getTipoRespuesta().startsWith(TIPO_RESPUESTA_TABLA)
+					|| pregunta.getTipoRespuesta().startsWith("MATRIZ")) {
 				crearRespuestaTablaMatriz(doc, pregunta);
 			}
 		}
 
 	}
 
+	/**
+	 * Crear la respuesta tabla o matriz asociada a una pregunta
+	 * 
+	 * @see #crearPreguntasPorArea(List, XWPFDocument)
+	 * @param doc
+	 * @param pregunta
+	 */
 	private void crearRespuestaTablaMatriz(XWPFDocument doc, PreguntasCuestionario pregunta) {
 		List<ConfiguracionRespuestasCuestionario> valoresColumnas = configuracionRespuestaRepository
 				.findColumnasBySeccion(pregunta.getTipoRespuesta());
 
 		XWPFTable table;
-		if (pregunta.getTipoRespuesta().startsWith("TABLA")) {
+		if (pregunta.getTipoRespuesta().startsWith(TIPO_RESPUESTA_TABLA)) {
 			table = doc.createTable(1, valoresColumnas.size());
 		}
 		else {
@@ -245,7 +295,7 @@ public class WordGenerator {
 		XWPFParagraph parrafoCelda;
 		for (int col = 0; col < valoresColumnas.size(); col++) {
 
-			if (pregunta.getTipoRespuesta().startsWith("TABLA")) {
+			if (pregunta.getTipoRespuesta().startsWith(TIPO_RESPUESTA_TABLA)) {
 				parrafoCelda = filaCabecera.getCell(col).addParagraph();
 			}
 			else {
@@ -273,6 +323,12 @@ public class WordGenerator {
 		}
 	}
 
+	/**
+	 * Crea el texto que se añadirá a un párrafo y se le da formato
+	 * @see #crearRespuestaTablaMatriz(XWPFDocument, PreguntasCuestionario)
+	 * @param parrafoCelda
+	 * @param valor
+	 */
 	private void setTextoYFormatoParrafoCelda(XWPFParagraph parrafoCelda, String valor) {
 		XWPFRun texto = parrafoCelda.createRun();
 		texto.setText(valor);
@@ -283,6 +339,13 @@ public class WordGenerator {
 		parrafoCelda.setVerticalAlignment(TextAlignment.CENTER);
 	}
 
+	/**
+	 * Obtiene un String con los posibles valores de un RADIO unidos por ", "
+	 * 
+	 * @see #crearPreguntasPorArea(List, XWPFDocument)
+	 * @param pregunta
+	 * @return String con los valores de un RADIO unidos por ", "
+	 */
 	private String getValoresRadioButton(PreguntasCuestionario pregunta) {
 		String valoresString = "";
 		List<String> valores = configuracionRespuestaRepository.findValoresPorSeccion(pregunta.getTipoRespuesta());
