ee7bbd037bdee32404dd42ccd0d97d6e9ed1cb44
AVANZIT\silvia.perez <silvia.perez@ezentis.com>
2017-02-07 14:39:00 +0100

    configuración para realizar las tareas programadas

--- src/main/java/es/mira/progesin/ProgesinApplication.java
+++ src/main/java/es/mira/progesin/ProgesinApplication.java
@@ -14,11 +14,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import es.mira.progesin.jsf.scope.FacesViewScope;
 
 // SpringBootApplication Equivale a @Configuration @EnableAutoConfiguration @ComponentScan
 @SpringBootApplication
+@EnableScheduling
 public class ProgesinApplication {
 
 	public static void main(String[] args) {
