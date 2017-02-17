6540fe0cc27119ed4d9595589a0349f79716d4c8
AVANZIT\rastudillo <ruben.astudillo@ezentis.com>
2017-02-17 12:32:23 +0100

    Añado una secuencia a la entity

--- src/main/java/es/mira/progesin/persistence/entities/CuerpoEstado.java
+++ src/main/java/es/mira/progesin/persistence/entities/CuerpoEstado.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.CreatedBy;
@@ -42,39 +43,40 @@ import lombok.ToString;
 @Entity
 @Table(name = "CUERPOSESTADO")
 public class CuerpoEstado implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "ID", length = 2)
-    private Integer id;
-    
-    @Column(name = "DESCRIPCION", length = 100, nullable = false)
-    private String descripcion;
-    
-    @Column(name = "nombre_corto", length = 10)
-    private String nombreCorto;
-    
-    @CreatedDate
-    @Column(name = "fecha_alta", nullable = false)
-    private Date fechaAlta;
-    
-    @CreatedBy
-    @Column(name = "username_alta", nullable = false)
-    private String usernameAlta;
-    
-    @LastModifiedDate
-    @Column(name = "fecha_modif")
-    private Date fechaModif;
-    
-    @LastModifiedBy
-    @Column(name = "username_modif")
-    private String usernameModif;
-    
-    @Column(name = "fecha_baja")
-    private Date fechaBaja;
-    
-    @Column(name = "username_baja", length = 12)
-    private String usernameBaja;
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name = "seq_cuerpos", sequenceName = "seq_cuerpos", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cuerpos")
+	@Column(name = "ID", length = 2)
+	private Integer id;
+
+	@Column(name = "DESCRIPCION", length = 100, nullable = false)
+	private String descripcion;
+
+	@Column(name = "nombre_corto", length = 10)
+	private String nombreCorto;
+
+	@CreatedDate
+	@Column(name = "fecha_alta", nullable = false)
+	private Date fechaAlta;
+
+	@CreatedBy
+	@Column(name = "username_alta", nullable = false)
+	private String usernameAlta;
+
+	@LastModifiedDate
+	@Column(name = "fecha_modif")
+	private Date fechaModif;
+
+	@LastModifiedBy
+	@Column(name = "username_modif")
+	private String usernameModif;
+
+	@Column(name = "fecha_baja")
+	private Date fechaBaja;
+
+	@Column(name = "username_baja", length = 12)
+	private String usernameBaja;
 }
