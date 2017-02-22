df510ab7e43fcffb0334c2d411a0cfb6b5816cf4
AVANZIT\rgomez <raul.gomez@ezentis.com>
2017-02-22 10:05:14 +0100

    Cambio SeccionesEnum.

--- src/main/java/es/mira/progesin/persistence/entities/enums/SeccionesEnum.java
+++ src/main/java/es/mira/progesin/persistence/entities/enums/SeccionesEnum.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum SeccionesEnum {
     ADMINISTRACION("ADMINISTRACIÓN"), DOCUMENTACION("SOLICITUD DOC. PREVIA"), CUESTIONARIO("CUESTIONARIOS"), GESTOR(
-            "GESTOR DOCUMENTAL"), GUIAS("GUÍAS"), INSPECCION("INSPECCIONES"), INFORMES("INFORMES");
+            "GESTOR DOCUMENTAL"), GUIAS("GUÍAS"), INSPECCION("INSPECCIONES"), INFORMES("INFORMES"), LOGIN("LOGIN");
     
     private String descripcion;
     
