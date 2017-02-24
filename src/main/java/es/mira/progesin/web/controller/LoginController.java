f01c39c788c6d55f3dc7bb56a0ef17312f013954
AVANZIT\rgarciao <ramon.garcia@ezentis.com>
2017-02-24 12:56:19 +0100

    Solucionada redirección a página de login cuando caduca la sesión y se intenta realizar alguna acción.

--- src/main/java/es/mira/progesin/web/controller/LoginController.java
+++ src/main/java/es/mira/progesin/web/controller/LoginController.java
@@ -1,5 +1,9 @@
 package es.mira.progesin.web.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -13,18 +17,28 @@ import lombok.Setter;
 @Setter
 @Controller
 public class LoginController {
-
-	private static final String LOGIN = "login";
-
-	@RequestMapping(method = RequestMethod.GET, value = "/login")
-	public String login() {
-		return LOGIN;
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return LOGIN;
-	}
-
+    
+    private static final String LOGIN = "login";
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/login")
+    public String login2(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if ("partial/ajax".equals(request.getHeader("Faces-Request"))) {
+            // JSF ajax request. Return special XML response which instructs JavaScript that it should in turn perform a
+            // redirect.
+            response.setContentType("text/xml");
+            response.getWriter().append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+                    .printf("<partial-response><redirect url=\"%s\"></redirect></partial-response>", LOGIN);
+            return null;
+        } else {
+            // Normal request. Perform redirect as usual.
+            return LOGIN;
+        }
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return LOGIN;
+    }
+    
 }
