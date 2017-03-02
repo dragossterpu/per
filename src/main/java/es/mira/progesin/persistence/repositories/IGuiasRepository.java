878e2ab22b26828f6106ddaf5be8c70f3c7c7aa3
AVANZIT\rastudillo <ruben.astudillo@ezentis.com>
2017-03-02 14:41:00 +0100

    Cambios en el repositorio

--- src/main/java/es/mira/progesin/persistence/repositories/IGuiasRepository.java
+++ src/main/java/es/mira/progesin/persistence/repositories/IGuiasRepository.java
@@ -1,9 +1,14 @@
 package es.mira.progesin.persistence.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import es.mira.progesin.persistence.entities.Guia;
 
 public interface IGuiasRepository extends CrudRepository<Guia, Long> {
+
+	@Override
+	List<Guia> findAll();
 
 }
