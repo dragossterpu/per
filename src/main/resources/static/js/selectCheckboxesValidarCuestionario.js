5b89e2a958c77ab5b64dd6045fc24dd3b66b0ce7
AVANZIT\rgarciao <ramon.garcia@ezentis.com>
2017-03-30 09:58:56 +0200

    Solucionado problema en sonar qube con el acceso al objeto javascript PrimeFaces por no respetar la jerarquía del DOM "window.PrimeFaces"

--- src/main/resources/static/js/selectCheckboxesValidarCuestionario.js
+++ src/main/resources/static/js/selectCheckboxesValidarCuestionario.js
@@ -12,30 +12,30 @@ $(document).ready(function(){
 	});
 	
 	function validarRespuestas(validarArea) {
-		var widgetArea = PrimeFaces.getWidgetById(validarArea.attr('id'));
+		var widgetArea = window.PrimeFaces.getWidgetById(validarArea.attr('id'));
 	    var checkedArea = widgetArea.input.prop("checked");
 	
 	    if (checkedArea) {
 	        validarArea.siblings('.prevPregCuest').find('.checkRespuesta input:enabled').each(function() {
-	        	PrimeFaces.getWidgetById($(this).closest('.checkRespuesta').attr('id')).check();
+	        	window.PrimeFaces.getWidgetById($(this).closest('.checkRespuesta').attr('id')).check();
 	        });
 	    } else {
 	        validarArea.siblings('.prevPregCuest').find('.checkRespuesta input:enabled').each(function() {
-	        	PrimeFaces.getWidgetById($(this).closest('.checkRespuesta').attr('id')).uncheck();
+	        	window.PrimeFaces.getWidgetById($(this).closest('.checkRespuesta').attr('id')).uncheck();
 	        });
 	    }
 	}
 					
 	function validarArea(validarRespuesta) {
-		var widgetRespuesta = PrimeFaces.getWidgetById(validarRespuesta.attr('id'));
+		var widgetRespuesta = window.PrimeFaces.getWidgetById(validarRespuesta.attr('id'));
 	    var checkedRespuesta = widgetRespuesta.input.prop("checked");
 	    var validarArea = validarRespuesta.closest('.prevPregCuest').siblings('.checkArea');
-	    var widgetArea = PrimeFaces.getWidgetById(validarArea.attr('id'));
+	    var widgetArea = window.PrimeFaces.getWidgetById(validarArea.attr('id'));
 	
 	    if (checkedRespuesta) {
 	    	var checkedArea = true;
 	        validarArea.siblings('.prevPregCuest').find('.checkRespuesta').each(function() {
-	        	checkedArea = checkedArea && PrimeFaces.getWidgetById($(this).attr('id')).input.prop("checked");
+	        	checkedArea = checkedArea && window.PrimeFaces.getWidgetById($(this).attr('id')).input.prop("checked");
 	        });
 	        if(checkedArea) { 
 	        	widgetArea.check(); 
--- src/main/webapp/resources/composites/selectManyCheckboxAll.js
+++ src/main/webapp/resources/composites/selectManyCheckboxAll.js
@@ -1,14 +1,14 @@
 $(document).on("click", ".checkboxes-all .all .ui-chkbox-box, .checkboxes-all .all input", function() {
     var $composite = $(this).closest(".checkboxes-all");
-    var widgetAll = PrimeFaces.widgets[$composite.data("widget-all")];
-    var widgetCheckboxes = PrimeFaces.widgets[$composite.data("widget-checkboxes")];
+    var widgetAll = window.PrimeFaces.widgets[$composite.data("widget-all")];
+    var widgetCheckboxes = window.PrimeFaces.widgets[$composite.data("widget-checkboxes")];
 
     widgetCheckboxes.inputs.prop("checked", !widgetAll.isChecked()).click();
 });
 
 $(document).on("click", ".checkboxes-all .ui-selectmanycheckbox input", function() {
     var $composite = $(this).closest(".checkboxes-all");
-    var widgetAll = PrimeFaces.widgets[$composite.data("widget-all")];
+    var widgetAll = window.PrimeFaces.widgets[$composite.data("widget-all")];
 
     if (!$(this).is(":checked") && widgetAll.isChecked()) {
         widgetAll.uncheck();
