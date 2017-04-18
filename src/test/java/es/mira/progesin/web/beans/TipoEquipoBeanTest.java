f1faf40c7773028298ad56cf8e6359dd907defd5
AVANZIT\rgarciao <ramon.garcia@ezentis.com>
2017-04-18 14:44:20 +0200

    Añadidas dependencias al pom:  - powermock permite simular clases estáticas y  sus métodos  - el-api elimina warnings referentes a la syntaxis EL en vistas y avisos al compilar de biblioteca no encontrada
    Añadido nuevo test para TipoEquipoBean

--- src/test/java/es/mira/progesin/web/beans/TipoEquipoBeanTest.java
+++ src/test/java/es/mira/progesin/web/beans/TipoEquipoBeanTest.java
@@ -3,142 +3,143 @@
  */
 package es.mira.progesin.web.beans;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.primefaces.event.RowEditEvent;
+
+import es.mira.progesin.persistence.entities.TipoEquipo;
+import es.mira.progesin.services.EquipoService;
+import es.mira.progesin.services.IRegistroActividadService;
+import es.mira.progesin.services.ITipoEquipoService;
+import es.mira.progesin.util.FacesUtilities;
 
 /**
- * @author Ezentis
+ * @author EZENTIS
+ * 
+ * Test del servicio Parametros
  *
  */
+// @RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ FacesUtilities.class })
 public class TipoEquipoBeanTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#tipoEquipoListado()}.
-	 */
-	@Test
-	public final void testTipoEquipoListado() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#eliminarTipo(es.mira.progesin.persistence.entities.TipoEquipo)}.
-	 */
-	@Test
-	public final void testEliminarTipo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#getFormNuevoTipoEquipo()}.
-	 */
-	@Test
-	public final void testGetFormNuevoTipoEquipo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#altaTipo()}.
-	 */
-	@Test
-	public final void testAltaTipo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#onRowEdit(org.primefaces.event.RowEditEvent)}.
-	 */
-	@Test
-	public final void testOnRowEdit() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#onRowCancel(org.primefaces.event.RowEditEvent)}.
-	 */
-	@Test
-	public final void testOnRowCancel() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#setTipoEquipoNuevo(TipoEquipo)}.
-	 */
-	@Test
-	public final void testSetTipoEquipoNuevo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#setListaTipoEquipo(List)}.
-	 */
-	@Test
-	public final void testSetListaTipoEquipo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#setTipoEquipoService(ITipoEquipoService)}.
-	 */
-	@Test
-	public final void testSetTipoEquipoService() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#getTipoEquipoNuevo()}.
-	 */
-	@Test
-	public final void testGetTipoEquipoNuevo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#getListaTipoEquipo()}.
-	 */
-	@Test
-	public final void testGetListaTipoEquipo() {
-		
-	}
-
-	/**
-	 * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#getTipoEquipoService()}.
-	 */
-	@Test
-	public final void testGetTipoEquipoService() {
-		
-	}
-
+    
+    @Mock
+    private FacesUtilities facesUtilities;
+    
+    @Mock
+    private ITipoEquipoService tipoEquipoService;
+    
+    @Mock
+    private EquipoService equipoService;
+    
+    @Mock
+    private transient IRegistroActividadService regActividadService;
+    
+    @InjectMocks
+    private TipoEquipoBean tipoEquipoBean;
+    
+    /**
+     * Configuración inicial del test
+     */
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(FacesUtilities.class);
+    }
+    
+    /**
+     * Comprobación clase existe
+     */
+    @Test
+    public void type() {
+        assertThat(TipoEquipoBean.class).isNotNull();
+    }
+    
+    /**
+     * Comprobación clase no abstracta
+     */
+    @Test
+    public void instantiation() {
+        TipoEquipoBean target = new TipoEquipoBean();
+        assertThat(target).isNotNull();
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#tipoEquipoListado()}.
+     */
+    @Test
+    public void tipoEquipoListado() {
+        tipoEquipoBean.tipoEquipoListado();
+        verify(tipoEquipoService, times(1)).findAll();
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#eliminarTipo(TipoEquipo)}.
+     */
+    @Test
+    public void eliminarTipo() {
+        TipoEquipo tEquipo = TipoEquipo.builder().id(1L).codigo("IAPRL")
+                .descripcion("Inspecciones Área Prevención de Riesgos Laborales").build();
+        List<TipoEquipo> listaTipoEquipo = new ArrayList<>();
+        listaTipoEquipo.add(tEquipo);
+        tipoEquipoBean.setListaTipoEquipo(listaTipoEquipo);
+        tipoEquipoBean.eliminarTipo(tEquipo);
+        verify(equipoService, times(1)).existsByTipoEquipo(tEquipo);
+        verify(tipoEquipoService, times(1)).delete(tEquipo.getId());
+        assertThat(tipoEquipoBean.getListaTipoEquipo().size()).isEqualTo(0);
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#init()}.
+     */
+    @Test
+    public void init() {
+        TipoEquipoBean tipoEquipoBeanMock = spy(tipoEquipoBean);
+        tipoEquipoBeanMock.init();
+        verify(tipoEquipoBeanMock, times(1)).tipoEquipoListado();
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#altaTipo(String, String)}.
+     */
+    @Test
+    public void altaTipo() {
+        TipoEquipo tEquipo = TipoEquipo.builder().codigo("TEST").descripcion("Tipo Equipo Test").build();
+        tipoEquipoBean.altaTipo("TEST", "Tipo Equipo Test");
+        ArgumentCaptor<TipoEquipo> tipoEquipoCaptor = ArgumentCaptor.forClass(TipoEquipo.class);
+        when(tipoEquipoService.save(tEquipo)).thenReturn(tEquipo);
+        verify(tipoEquipoService, times(1)).save(tipoEquipoCaptor.capture());
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.TipoEquipoBean#onRowEdit(RowEditEvent)}.
+     */
+    @Test
+    public void onRowEdit() {
+        TipoEquipo tEquipo = TipoEquipo.builder().codigo("TEST").descripcion("Tipo Equipo Test").build();
+        RowEditEvent event = mock(RowEditEvent.class);
+        when(event.getObject()).thenReturn(tEquipo);
+        tipoEquipoBean.onRowEdit(event);
+        ArgumentCaptor<TipoEquipo> tipoEquipoCaptor = ArgumentCaptor.forClass(TipoEquipo.class);
+        when(tipoEquipoService.save(tEquipo)).thenReturn(tEquipo);
+        verify(tipoEquipoService, times(1)).save(tipoEquipoCaptor.capture());
+    }
+    
 }
