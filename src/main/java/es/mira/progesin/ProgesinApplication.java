407b3b0ec195260d1475bf22107063c952013940
AVANZIT\rgarciao <ramon.garcia@ezentis.com>
2017-05-08 13:04:17 +0200

    Añadida página error 403 acceso denegado

--- src/main/java/es/mira/progesin/ProgesinApplication.java
+++ src/main/java/es/mira/progesin/ProgesinApplication.java
@@ -52,7 +52,7 @@ public class ProgesinApplication {
         
         @Override
         public void registerErrorPages(ErrorPageRegistry registry) {
-            registry.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "redirect:/index.xhtml"),
+            registry.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/error/403.xhtml"),
                     new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.xhtml"),
                     new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/5xx.xhtml"),
                     new ErrorPage(HttpStatus.BAD_GATEWAY, "/error/5xx.xhtml"));
