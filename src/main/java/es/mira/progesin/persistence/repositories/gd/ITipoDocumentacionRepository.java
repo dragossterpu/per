61a63cbf4f24b2dc88bc93d133ff15f2bff3c745
AVANZIT\rgomez <raul.gomez@ezentis.com>
2017-05-24 09:01:45 +0200

    Test ITipoDocumentacionRepository.

--- src/main/java/es/mira/progesin/persistence/repositories/gd/ITipoDocumentacionRepository.java
+++ src/main/java/es/mira/progesin/persistence/repositories/gd/ITipoDocumentacionRepository.java
@@ -7,8 +7,18 @@ import org.springframework.data.repository.CrudRepository;
 import es.mira.progesin.persistence.entities.enums.AmbitoInspeccionEnum;
 import es.mira.progesin.persistence.entities.gd.TipoDocumentacion;
 
+/**
+ * Repositorio del tipo de documentación
+ * 
+ * @author EZENTIS
+ *
+ */
 public interface ITipoDocumentacionRepository extends CrudRepository<TipoDocumentacion, Long> {
     
+    /**
+     * @param ambito
+     * @return lista de tipos de documentación por ámbito
+     */
     List<TipoDocumentacion> findByAmbito(AmbitoInspeccionEnum ambito);
     
 }
---
+++ src/test/java/es/mira/progesin/persistence/repositories/ITipoDocumentacionRepositoryTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package es.mira.progesin.persistence.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import es.mira.progesin.persistence.entities.enums.AmbitoInspeccionEnum;
+import es.mira.progesin.persistence.entities.gd.TipoDocumentacion;
+import es.mira.progesin.persistence.repositories.gd.ITipoDocumentacionRepository;
+
+/**
+ * Test del repositorio de tipo de documentación
+ * 
+ * @author EZENTIS
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@TestPropertySource(locations = "classpath:test.properties")
+public class ITipoDocumentacionRepositoryTest {
+    
+    @Autowired
+    private ITipoDocumentacionRepository repository;
+    
+    /**
+     * Test method for
+     * {@link es.mira.progesin.persistence.repositories.gd.ITipoDocumentacionRepository#findByAmbito(es.mira.progesin.persistence.entities.enums.AmbitoInspeccionEnum)}.
+     */
+    @Test
+    public void testFindByAmbito() {
+        List<TipoDocumentacion> listTipoDoc = repository.findByAmbito(AmbitoInspeccionEnum.PN);
+        assertThat(listTipoDoc.size()).isEqualTo(11);
+    }
+    
+}
