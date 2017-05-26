95af74088e9baa379226f8e7fc95c46454be611d
AVANZIT\rgomez <raul.gomez@ezentis.com>
2017-05-26 10:56:06 +0200

    Javadoc interfaz servicio inspecciones.

--- src/main/java/es/mira/progesin/services/IInspeccionesService.java
+++ src/main/java/es/mira/progesin/services/IInspeccionesService.java
@@ -10,25 +10,28 @@ import es.mira.progesin.persistence.entities.enums.EstadoInspeccionEnum;
 import es.mira.progesin.web.beans.InspeccionBusqueda;
 
 /**
- * @author Ezentis
+ * Interfaz del servicio de inspeciones.
+ * @author EZENTIS
  *
  */
 public interface IInspeccionesService {
     
     /**
-     * Método que guarda una inspección
+     * Método que guarda una inspección.
      * @param inspecciones a guardar
-     * @return devuelve el resultado del guardado de la inspección
+     * @return devuelve la inspección guardada
      */
     public Inspeccion save(Inspeccion inspecciones);
     
     /**
-     * Borra una inspección pasada por parámetro
+     * Borra una inspección pasada por parámetro.
      * @param inspecciones a borrar
      */
     public void delete(Inspeccion inspecciones);
     
     /**
+     * Busca inspecciones no finalizadas filtrando por el nombre de la unidad o el número de la inspección.
+     * 
      * @param infoInspeccion nombre de la unidad o número de inspección
      * @return devuelve una lista con todas las inspecciones filtradas indicando el nombre de la unidad o el número de
      * inspección
@@ -36,6 +39,8 @@ public interface IInspeccionesService {
     List<Inspeccion> buscarNoFinalizadaPorNombreUnidadONumero(String infoInspeccion);
     
     /**
+     * Busca inspecciones filtrando por el nombre de la unidad o el número de la inspección.
+     * 
      * @param infoInspeccion nombre de la unidad o número de inspección
      * @return devuelve una lista con todas las inspecciones filtradas indicando el nombre de la unidad o el número de
      * inspección
@@ -53,46 +58,52 @@ public interface IInspeccionesService {
     
     /**
      * @param busqueda bean InspeccionBusqueda que define el filtro de la consulta realizada
-     * @return busqueda bean InspeccionBusqueda que define el filtro de la consulta realizada
+     * @return búsqueda bean InspeccionBusqueda que define el filtro de la consulta realizada
      */
     int getCountInspeccionCriteria(InspeccionBusqueda busqueda);
     
     /**
-     * @param id d la inspección
-     * @return devuelve inspección si es encontrada
+     * Busca una inspección con cierto id pasado por parámetro.
+     * 
+     * @param id de la inspección
+     * @return devuelve inspección si es encontrada.
      */
     public Inspeccion findInspeccionById(Long id);
     
     /**
+     * Devuelve la lista de inspecciones asociadas a otra.
+     * 
      * @param inspeccion pasada por parámetro
-     * @return devuelve lista de inspecciones asociadas a la que se pasa por parámetro
+     * @return devuelve lista de inspecciones asociadas
      */
     public List<Inspeccion> listaInspeccionesAsociadas(Inspeccion inspeccion);
     
     /**
+     * Comprueba si existe una inspección de determinado tipo.
+     * 
      * @param tipo de inspección
-     * @return true o false dependiendo de si existe una inspección del tipo pasado por parámetro
+     * @return valor booleando de la comprobación
      */
     boolean existeByTipoInspeccion(TipoInspeccion tipo);
     
     /**
-     * Método que realiza una consulta de inspecciones a través criteria coincidente con determinados parámetros
+     * Método que realiza una consulta de inspecciones, usando criteria, coincidente con determinados parámetros.
      * 
      * @param first primer registro
      * @param pageSize tamaño de la página (número de registros que queremos)
      * @param sortField campo por el que ordenamos
      * @param sortOrder si la ordenación es ascendente o descendente
      * @param busqueda bean InspeccionBusqueda que define el filtro de la consulta realizada
-     * @return resultado de la consulta
+     * @return lista de inspecciones resultado de la consulta
      */
     List<Inspeccion> buscarInspeccionPorCriteria(int first, int pageSize, String sortField, SortOrder sortOrder,
             InspeccionBusqueda busqueda);
     
     /**
-     * Cambia el estado de una inspección
+     * Cambia el estado de una inspección.
      * 
-     * @param inspeccion
-     * @param estado
+     * @param inspeccion a cambiar
+     * @param estado a poner
      */
     void cambiarEstado(Inspeccion inspeccion, EstadoInspeccionEnum estado);
     
