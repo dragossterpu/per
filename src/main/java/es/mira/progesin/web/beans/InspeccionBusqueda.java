ed26d714dd846cf4751a86c60506209196025903
AVANZIT\rgomez <raul.gomez@ezentis.com>
2017-05-29 13:03:46 +0200

    Comentarios del bean de búsqueda de inspecciones.

--- src/main/java/es/mira/progesin/web/beans/InspeccionBusqueda.java
+++ src/main/java/es/mira/progesin/web/beans/InspeccionBusqueda.java
@@ -18,6 +18,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
+ * Bean usado como filtro para el buscador de inspeciones.
+ * 
  * @author EZENTIS
  *
  */
@@ -27,46 +29,104 @@ public class InspeccionBusqueda implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
+    /**
+     * Fecha desde la que la búsqueda incluye inspecciones.
+     */
     private Date fechaDesde;
     
+    /**
+     * Fecha hasta la que la búsqueda incluye inspecciones.
+     */
     private Date fechaHasta;
     
+    /**
+     * Filtro usuario de creación.
+     */
     private String usuarioCreacion;
     
+    /**
+     * Filtro año de la inspección.
+     */
     private String anio;
     
+    /**
+     * Filtro equipo.
+     */
     private Equipo equipo;
     
+    /**
+     * Filtro tipo de inspección.
+     */
     private TipoInspeccion tipoInspeccion;
     
+    /**
+     * Filtro nombre de la unidad.
+     */
     private String nombreUnidad;
     
+    /**
+     * Filtro jefe de equipo.
+     */
     private String jefeEquipo;
     
+    /**
+     * Filtro identificador.
+     */
     private String id;
     
+    /**
+     * Filtro ámbito.
+     */
     private AmbitoInspeccionEnum ambito;
     
+    /**
+     * Filtro cuatrimestre.
+     */
     private CuatrimestreEnum cuatrimestre;
     
+    /**
+     * Filtro stado.
+     */
     private EstadoInspeccionEnum estado;
     
+    /**
+     * Filtro provincia.
+     */
     private Provincia provincia;
     
+    /**
+     * Filtro municipio.
+     */
     private Municipio municipio;
     
+    /**
+     * Filtro tipo de unidad.
+     */
     private TipoUnidad tipoUnidad;
     
+    /**
+     * Campo de ordenación en la tabla de inspecciones.
+     */
     private String sortField;
     
+    /**
+     * Orden de ordenación en la tabla de inspecciones (ascendente o descendente).
+     */
     private SortOrder sortOrder;
     
+    /**
+     * Inspección que se esta modificando o creando en el momento. Es la que se muestra en las vistas.
+     */
     private Inspeccion inspeccionModif;
     
+    /**
+     * Distingue entre la vista de búsqueda de inspecciones y de asociar inspecciones ya que se emplea el mismo xhtml
+     * para ambas.
+     */
     private boolean asociar;
     
     /**
-     * Limpia los valores del objeto
+     * Limpia los valores del objeto de búsqueda.
      * 
      */
     public void resetValues() {
