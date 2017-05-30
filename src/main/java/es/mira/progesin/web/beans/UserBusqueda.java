40a282e96132747dc74ee885cd99c590e69b8424
AVANZIT\rgomez <raul.gomez@ezentis.com>
2017-05-30 14:30:23 +0200

    Comentarios BeanBusqueda.

--- src/main/java/es/mira/progesin/web/beans/UserBusqueda.java
+++ src/main/java/es/mira/progesin/web/beans/UserBusqueda.java
@@ -29,30 +29,63 @@ public class UserBusqueda implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
+    /**
+     * Fecha desde la que la búsqueda incluye inspecciones.
+     */
     private Date fechaDesde;
     
+    /**
+     * Fecha hasta la que la búsqueda incluye inspecciones.
+     */
     private Date fechaHasta;
     
+    /**
+     * Filtro username.
+     */
     private String username;
     
+    /**
+     * Filtro nombre user.
+     */
     private String nombre;
     
+    /**
+     * Filtro primer apellido user.
+     */
     private String apellido1;
     
+    /**
+     * Filtro segundo apellido user.
+     */
     private String apellido2;
     
+    /**
+     * Filtro rol user.
+     */
     private RoleEnum role;
     
+    /**
+     * Filtro estado user.
+     */
     private EstadoEnum estado;
     
+    /**
+     * Filtro cuerpo al que pertenece el user.
+     */
     private CuerpoEstado cuerpoEstado;
     
+    /**
+     * Filtro puesto user.
+     */
     private PuestoTrabajo puestoTrabajo;
     
+    /**
+     * Lista de usuarios de la búsqueda.
+     */
     private List<User> listaUsuarios;
     
     /**
-     * Resetea los valores del formulario de búsqueda de usuarios
+     * Resetea los valores del formulario de búsqueda de usuarios.
      */
     public void resetValues() {
         setFechaDesde(null);
