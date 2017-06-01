ec7c0c14ea7c7a2043bf7d86515a2a6c3672c626
AVANZIT\silvia.perez <silvia.perez@ezentis.com>
2017-06-01 15:36:34 +0200

    javadoc y borrado de método que no se usa

--- src/main/java/es/mira/progesin/persistence/repositories/IDatosTablaGenericaRepository.java
+++ src/main/java/es/mira/progesin/persistence/repositories/IDatosTablaGenericaRepository.java
@@ -6,8 +6,17 @@ import org.springframework.data.repository.CrudRepository;
 
 import es.mira.progesin.model.DatosTablaGenerica;
 
+/**
+ * Repositorio donde se almacenan las respuestas de un cuestionario que son de tipo TABLA o MATRIZ.
+ * 
+ * @author EZENTIS
+ *
+ */
 public interface IDatosTablaGenericaRepository extends CrudRepository<DatosTablaGenerica, Long> {
     
+    /**
+     * Borra las respuestas huérfanas.
+     */
     @Modifying(clearAutomatically = true)
     @Query(value = "delete from respuesta_datos_tabla r where r.respuesta_id_cuest_enviado is null and r.respuesta_id_pregunta is null", nativeQuery = true)
     void deleteRespuestasTablaHuerfanas();
--- src/main/java/es/mira/progesin/persistence/repositories/IPreguntaCuestionarioRepository.java
+++ src/main/java/es/mira/progesin/persistence/repositories/IPreguntaCuestionarioRepository.java
@@ -9,24 +9,49 @@ import org.springframework.data.repository.query.Param;
 import es.mira.progesin.persistence.entities.cuestionarios.AreasCuestionario;
 import es.mira.progesin.persistence.entities.cuestionarios.PreguntasCuestionario;
 
+/**
+ * Repositorio de preguntas de cuestionario.
+ * 
+ * @author EZENTIS
+ *
+ */
 public interface IPreguntaCuestionarioRepository extends CrudRepository<PreguntasCuestionario, Long> {
     
+    /**
+     * Busca las preguntas activas de un área ordenadas de forma ascendente por el campo orden.
+     * 
+     * @param area área de la que deseamos obtener sus preguntas
+     * @return lista de preguntas
+     */
     List<PreguntasCuestionario> findByAreaAndFechaBajaIsNullOrderByOrdenAsc(AreasCuestionario area);
     
+    /**
+     * Busca las preguntas de un cuestionario personalizado.
+     * 
+     * @param idCuestionarioPersonalizado id del cuestionario personalizado
+     * @return lista de preguntas
+     */
     @Query("select c.preguntasElegidas from CuestionarioPersonalizado c where c.id = :idCuestionarioPersonalizado")
     List<PreguntasCuestionario> findPreguntasElegidasCuestionarioPersonalizado(
             @Param("idCuestionarioPersonalizado") Long idCuestionarioPersonalizado);
     
-    @Query(value = "select p.* from CUESTIONARIO_PERSONALIZADO c, CUEST_PER_PREGUNTAS cp, PREGUNTASCUESTIONARIO p "
-            + "where c.id = cp.ID_CUEST_PERS and cp.ID_PREG_ELEGIDA = p.id and (p.TIPO_RESPUESTA like 'TABLA%' or p.TIPO_RESPUESTA like 'MATRIZ%') "
-            + "and c.id = :idCuestionarioPersonalizado", nativeQuery = true)
-    List<PreguntasCuestionario> findPreguntasElegidasTablaMatrizCuestionarioPersonalizado(
-            @Param("idCuestionarioPersonalizado") Long idCuestionarioPersonalizado);
-    
+    /**
+     * Busca si una pregunta está siendo usado en algún cuestionario personalizado.
+     * 
+     * @param idPregunta id de la pregunta
+     * @return pregunta
+     */
     @Query(value = "select distinct p.* from preguntascuestionario p, cuest_per_preguntas cpp, cuestionario_personalizado cp "
             + "where p.id = cpp.id_preg_elegida and cpp.id_cuest_pers = cp.id and p.id = ?1", nativeQuery = true)
     PreguntasCuestionario findPreguntaExistenteEnCuestionariosPersonalizados(Long idPregunta);
     
+    /**
+     * Busca las preguntas asociadas de un serie de áreas de un cuestionario personalizado.
+     * 
+     * @param idCuestionarioPersonalizado id del cuestionario personalizado
+     * @param listaIdAreasCuestionario listado de id de áreas
+     * @return listado de preguntas
+     */
     @Query(value = "select p.* from CUESTIONARIO_PERSONALIZADO c, CUEST_PER_PREGUNTAS cp, PREGUNTASCUESTIONARIO p "
             + "where c.id = cp.ID_CUEST_PERS and cp.ID_PREG_ELEGIDA = p.id and p.id_area in (?2) "
             + "and c.id = ?1", nativeQuery = true)
