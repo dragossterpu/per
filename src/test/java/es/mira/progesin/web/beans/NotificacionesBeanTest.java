9222d384eb5c9b29436af5f6998b3d438232e144
AVANZIT\rgomez <raul.gomez@ezentis.com>
2017-08-01 14:26:38 +0200

    Test NotificacionesBean.

---
+++ src/test/java/es/mira/progesin/web/beans/NotificacionesBeanTest.java
@@ -0,0 +1,137 @@
+/**
+ * 
+ */
+package es.mira.progesin.web.beans;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.primefaces.event.ToggleEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import es.mira.progesin.persistence.entities.Notificacion;
+import es.mira.progesin.persistence.entities.enums.SeccionesEnum;
+import es.mira.progesin.persistence.entities.enums.TipoRegistroEnum;
+import es.mira.progesin.services.IRegistroActividadService;
+import es.mira.progesin.util.FacesUtilities;
+
+/**
+ * Test para el servicio de notificaciones.
+ * @author EZENTIS
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.security.*")
+@PrepareForTest({ FacesUtilities.class, SecurityContextHolder.class })
+public class NotificacionesBeanTest {
+    
+    /**
+     * Simulación del securityContext.
+     */
+    @Mock
+    private SecurityContext securityContext;
+    
+    /**
+     * Simulación de la autenticación.
+     */
+    @Mock
+    private Authentication authentication;
+    
+    /**
+     * Mock Repositorio de alertas.
+     */
+    @Mock
+    private IRegistroActividadService regActividad;
+    
+    /**
+     * Simulación del Gestor Documental.
+     */
+    @InjectMocks
+    private NotificacionesBean alertasBeanMock;
+    
+    /**
+     * Comprobación clase existe.
+     */
+    @Test
+    public void type() {
+        assertThat(NotificacionesBean.class).isNotNull();
+    }
+    
+    /**
+     * Comprobación clase no abstracta.
+     */
+    @Test
+    public void instantiation() {
+        NotificacionesBean target = new NotificacionesBean();
+        assertThat(target).isNotNull();
+    }
+    
+    /**
+     * Configuración inicial del test.
+     */
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(FacesUtilities.class);
+        PowerMockito.mockStatic(SecurityContextHolder.class);
+        when(SecurityContextHolder.getContext()).thenReturn(securityContext);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("usuarioLogueado");
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.NotificacionesBean#onToggle(org.primefaces.event.ToggleEvent)}.
+     */
+    @Test
+    public final void testOnToggle() {
+        ToggleEvent eventMock = mock(ToggleEvent.class);
+        when(eventMock.getData()).thenReturn(0);
+        List<Boolean> listaToogle = new ArrayList<>();
+        listaToogle.add(Boolean.FALSE);
+        alertasBeanMock.setList(listaToogle);
+        alertasBeanMock.onToggle(eventMock);
+        assertThat(listaToogle.get(0)).isFalse();
+    }
+    
+    /**
+     * Test method for
+     * {@link es.mira.progesin.web.beans.NotificacionesBean#eliminarNotificacion(es.mira.progesin.persistence.entities.Notificacion)}.
+     */
+    @Test
+    public final void testEliminarNotificacion() {
+        Notificacion notif = new Notificacion();
+        alertasBeanMock.eliminarNotificacion(notif);
+        verify(regActividad, times(1)).altaRegActividad(any(String.class), eq(TipoRegistroEnum.BAJA.name()),
+                eq(SeccionesEnum.NOTIFICACIONES.getDescripcion()));
+        assertThat(notif.getFechaBaja()).isNotNull();
+        assertThat(notif.getUsernameBaja()).isNotNull();
+    }
+    
+    /**
+     * Test method for {@link es.mira.progesin.web.beans.NotificacionesBean#init()}.
+     */
+    @Test
+    public final void testInit() {
+        alertasBeanMock.init();
+        assertThat(alertasBeanMock.getList()).hasSize(5);
+    }
+    
+}
